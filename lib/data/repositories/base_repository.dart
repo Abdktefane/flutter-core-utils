2a3c3fe7554497be606ea770778a45c240416404
abd ktefane <abd.ktefane.7@gmail.com>
2021-03-17 21:44:29 +0200

    change base respository

--- lib/data/repositories/base_repository.dart
+++ lib/data/repositories/base_repository.dart
@@ -1,31 +1,7 @@
 import 'package:core_sdk/data/datasource/base_remote_data_source.dart';
-import 'package:meta/meta.dart';
-import 'package:data_connection_checker/data_connection_checker.dart';
-/* 
+
 abstract class BaseRepository {
-  /// Check if the
-  Either<Failure, T> checkResponse<T>(EitherFailureOr<T> body, String data);
-}
+  const BaseRepository(this.baseRemoteDataSource);
 
-class BaseRepositoryImpl extends BaseRepository {
-  final DataConnectionChecker connectionChecker;
   final BaseRemoteDataSource baseRemoteDataSource;
-
-  BaseRepositoryImpl({
-    @required this.connectionChecker,
-    @required this.baseRemoteDataSource,
-  });
-
-  // TODO(abd): change this into NETWORK RESULT
-  @override
-  Either<Failure, T> checkResponse<T>(EitherFailureOr<T> body, String data) {
-    try {
-      return body();
-    } catch (e) {
-      return Left(UnknownFailure(
-        'msg_something_wrong',
-        // json.decode(data)['message'],
-      ));
-    }
-  }
-} */
+}

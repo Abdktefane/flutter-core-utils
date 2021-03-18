6a5359f733f1e84e7a3a2d423588e9fd40e526d7
abd ktefane <abd.ktefane.7@gmail.com>
2021-03-18 11:18:17 +0200

    add map marker function

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,7 +1,14 @@
+import 'dart:async';
+import 'dart:typed_data';
+
 import 'package:core_sdk/utils/app_localizations.dart';
 import 'package:core_sdk/utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import 'dart:ui' as ui;
 
 String dateFormatter(String dateStr, AppLocalizations localizations) {
   if (dateStr == null || dateStr.isEmpty) return '';
@@ -29,6 +36,47 @@ String durationToFormattedTime(Duration time, Locale locale) {
   return formattedTime;
 }
 
+bool isArabic({BuildContext context, AppLocalizations localizations}) {
+  assert(context != null || localizations != null);
+  return (localizations ?? AppLocalizations.of(context)).locale.languageCode == LANGUAGE_ARABIC;
+}
+
+Future<BitmapDescriptor> getMapMarker(String assetPath) async {
+  final data = await rootBundle.load(assetPath);
+  final list = Uint8List.view(data.buffer);
+  final completer = Completer<ui.Image>();
+  ui.decodeImageFromList(list, completer.complete);
+  return getMarkerIconWithTitle(await completer.future);
+}
+
+Future<BitmapDescriptor> getMarkerIconWithTitle(ui.Image image) async {
+  final size = Size(400, 200);
+  final ui.PictureRecorder pictureRecorder = ui.PictureRecorder();
+  final Canvas canvas = Canvas(pictureRecorder);
+  final double shadowWidth = 15.0;
+
+//  final Paint borderPaint = Paint()..color = Colors.white;
+  final double borderWidth = 3.0;
+
+  final double imageOffset = shadowWidth + borderWidth;
+
+  // Oval for the image
+  Rect oval =
+      Rect.fromLTWH(imageOffset, imageOffset + 20, size.width - (imageOffset * 2), size.height - (imageOffset * 2));
+
+  // Add image
+  paintImage(canvas: canvas, image: image, rect: oval, fit: BoxFit.contain);
+
+  // Convert canvas to image
+  final ui.Image markerAsImage = await pictureRecorder.endRecording().toImage(size.width.toInt(), size.height.toInt());
+
+  // Convert image to bytes
+  final ByteData byteData = await markerAsImage.toByteData(format: ui.ImageByteFormat.png);
+  final Uint8List uint8List = byteData.buffer.asUint8List();
+
+  return BitmapDescriptor.fromBytes(uint8List);
+}
+
 // Future<String> findLocalPath({bool forDownload = false}) async {
 //   final directory = Platform.isAndroid
 //       ? forDownload
@@ -36,48 +84,4 @@ String durationToFormattedTime(Duration time, Locale locale) {
 //           : await getExternalStorageDirectory()
 //       : await getApplicationDocumentsDirectory();
 //   return directory.path;
-// }
-
-bool isArabic({BuildContext context, AppLocalizations localizations}) {
-  assert(context != null || localizations != null);
-  return (localizations ?? AppLocalizations.of(context)).locale.languageCode == LANGUAGE_ARABIC;
-}
-
-// Future<BitmapDescriptor> getMapMarker(String assetPath) async {
-//   final data = await rootBundle.load(assetPath);
-//   final list = Uint8List.view(data.buffer);
-//   final completer = Completer<ui.Image>();
-//   ui.decodeImageFromList(list, completer.complete);
-//   return getMarkerIconWithTitle(await completer.future);
-// }
-
-// Future<BitmapDescriptor> getMarkerIconWithTitle(ui.Image image) async {
-//   final size = Size(400, 200);
-//   final ui.PictureRecorder pictureRecorder = ui.PictureRecorder();
-//   final Canvas canvas = Canvas(pictureRecorder);
-//   final double shadowWidth = 15.0;
-
-// //  final Paint borderPaint = Paint()..color = Colors.white;
-//   final double borderWidth = 3.0;
-
-//   final double imageOffset = shadowWidth + borderWidth;
-
-//   // Oval for the image
-//   Rect oval = Rect.fromLTWH(imageOffset, imageOffset + 20,
-//       size.width - (imageOffset * 2), size.height - (imageOffset * 2));
-
-//   // Add image
-//   paintImage(canvas: canvas, image: image, rect: oval, fit: BoxFit.contain);
-
-//   // Convert canvas to image
-//   final ui.Image markerAsImage = await pictureRecorder
-//       .endRecording()
-//       .toImage(size.width.toInt(), size.height.toInt());
-
-//   // Convert image to bytes
-//   final ByteData byteData =
-//       await markerAsImage.toByteData(format: ui.ImageByteFormat.png);
-//   final Uint8List uint8List = byteData.buffer.asUint8List();
-
-//   return BitmapDescriptor.fromBytes(uint8List);
 // }

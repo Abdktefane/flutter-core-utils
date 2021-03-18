7ac7478fcafcbddb49a947ef8b5450f2f34868ea
abd ktefane <abd.ktefane.7@gmail.com>
2021-03-18 14:55:49 +0200

    remove DI from core

--- lib/utils/Fimber/logger_impl.dart
+++ lib/utils/Fimber/logger_impl.dart
@@ -1,11 +1,9 @@
 import 'package:fimber/fimber.dart';
 import 'package:flutter/foundation.dart';
-import 'package:injectable/injectable.dart';
 
 import 'CrashReportingTree.dart';
 import 'Logger.dart';
 
-@Singleton(as: Logger)
 class LoggerImpl extends Logger {
   LoggerImpl() {
     final LogTree tree = kReleaseMode ? CrashReportingTree() : DebugTree();

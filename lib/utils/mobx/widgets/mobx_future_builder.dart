df15e1117c4b103306aa8e9f0447a42c69a771fc
abd ktefane <abd.ktefane.7@gmail.com>
2021-04-29 12:58:57 +0300

    add animation switcher to MobxFutureBuilder and custom duration

--- lib/utils/mobx/widgets/mobx_future_builder.dart
+++ lib/utils/mobx/widgets/mobx_future_builder.dart
@@ -10,24 +10,36 @@ class MobxFutureBuilder<T> extends StatelessWidget {
     @required this.onSuccess,
     @required this.onError,
     @required this.onLoading,
+    this.animatedSwitcherDuration = const Duration(milliseconds: 500),
   }) : super(key: key);
 
   final ObservableFuture<T> future;
   final Widget Function(dynamic) onError;
   final Widget Function() onLoading;
   final Widget Function(T) onSuccess;
+  final Duration animatedSwitcherDuration;
 
   @override
   Widget build(BuildContext context) {
     return Observer(
       builder: (_) {
-        if (future.isFailure) return onError(future.error);
+        return AnimatedSwitcher(
+          duration: animatedSwitcherDuration,
+          child: future.isFailure
+              ? onError(future.error)
+              : future.isPending
+                  ? onLoading()
+                  : future.isSuccess
+                      ? onSuccess(future.value)
+                      : SizedBox(),
+        );
+        // if (future.isFailure) return onError(future.error);
 
-        if (future.isPending) return onLoading();
+        // if (future.isPending) return onLoading();
 
-        if (future.isSuccess) return onSuccess(future.value);
+        // if (future.isSuccess) return onSuccess(future.value);
 
-        return SizedBox();
+        // return SizedBox();
       },
     );
   }

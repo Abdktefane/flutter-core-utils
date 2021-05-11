46311f1b85a4d784481190821a9bea0d6690da64
abd ktefane <abd.ktefane.7@gmail.com>
2021-05-11 18:52:05 +0300

    fix bugs

--- lib/utils/extensions/future.dart
+++ lib/utils/extensions/future.dart
@@ -2,12 +2,12 @@ import 'dart:async';
 
 import 'package:core_sdk/utils/network_result.dart';
 
-extension FutureExtension<T> on Future<NetworkResult<T>> {
-  Future<NetworkResult<R>> whenSuccessWrapped<R>(FutureOr<R> Function(T res) onSuccess) {
-    return then((res) => res.getOrThrow()).then((res) => onSuccess(res)).then((res) => Success(res));
+extension FutureExtension<T> on Future<NetworkResult<T?>> {
+  Future<NetworkResult<R?>> whenSuccessWrapped<R>(FutureOr<R> Function(T res) onSuccess) {
+    return then((res) => res.getOrThrow()).then((res) => onSuccess(res!)).then((res) => Success(res));
   }
 
-  Future<R> whenSuccess<R>(FutureOr<R> Function(T res) onSuccess) {
-    return then((res) => res.getOrThrow()).then((res) => onSuccess(res));
+  Future<R?> whenSuccess<R>(FutureOr<R> Function(T res) onSuccess) {
+    return then((res) => res.getOrThrow()).then((res) => onSuccess(res!));
   }
 }

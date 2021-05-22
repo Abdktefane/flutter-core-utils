ff06546ef3a649cd23e8dbf0e0ef66fb107690e0
abd ktefane <abd.ktefane.7@gmail.com>
2021-05-22 16:24:46 +0300

    fix bugs

--- lib/utils/extensions/list.dart
+++ lib/utils/extensions/list.dart
@@ -1,3 +1,7 @@
 extension ListExtension<T> on List<T> {
   bool get isNullOrEmpty => this == null || isEmpty;
 }
+
+extension ListExtension2<T> on List<T>? {
+  bool get isNullOrEmpty => this == null || this!.isEmpty;
+}
--- lib/utils/extensions/string.dart
+++ lib/utils/extensions/string.dart
@@ -5,5 +5,14 @@ extension StringExtension on String {
   bool get isNullOrEmpty => this == null || isEmpty;
   static DateFormat formatter = DateFormat.yMMMd();
 
-  static String? asDate(String input) => input == null ? null : DateFormat.yMMMd().format(DateTime.parse(input));
+  static String? asDate(String? input) => input == null ? null : DateFormat.yMMMd().format(DateTime.parse(input));
+}
+
+extension StringExtension2 on String? {
+  // DateFormat get monthFormatter => DateFormat.MMMd();
+  bool get isNullOrEmpty => this == null || this!.isEmpty;
+
+  static DateFormat formatter = DateFormat.yMMMd();
+
+  static String? asDate(String? input) => input == null ? null : DateFormat.yMMMd().format(DateTime.parse(input));
 }

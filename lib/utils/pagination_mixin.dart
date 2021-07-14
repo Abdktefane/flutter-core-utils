9a95895503a970747e25e188f5b017dae6ad745c
abd ktefane <abd.ktefane.7@gmail.com>
2021-07-14 16:41:22 +0300

    modify paginatin mixin stratigy

--- lib/utils/pagination_mixin.dart
+++ lib/utils/pagination_mixin.dart
@@ -3,7 +3,6 @@ import 'package:flutter/cupertino.dart';
 mixin PaginationMixin {
   late ScrollController scrollController;
   late double percent;
-  // final processingSet = <int>{};
   int lastNumberOfPages = 0;
 
   void initPagination({double percent = 0.5}) {
@@ -16,23 +15,29 @@ mixin PaginationMixin {
   }
 
   void _handleScrollListner() {
-    // if (scrollController.offset >= scrollController.position.maxScrollExtent && !scrollController.position.outOfRange) {
-    //   debouncer.run(onLoadMore);
-    // }
     final double max = scrollController.position.maxScrollExtent;
     final double viewport = scrollController.position.viewportDimension;
     final double before = scrollController.position.extentBefore;
     final int numberOfPages = max ~/ viewport;
-    final int currentPage = before ~/ viewport;
-    if (currentPage / numberOfPages >= percent &&
-            !scrollController.position.outOfRange &&
-            lastNumberOfPages != numberOfPages //&&
-        // !processingSet.contains(currentPage)
-        ) {
+    if (before > (max / 2) && !scrollController.position.outOfRange && lastNumberOfPages != numberOfPages) {
       lastNumberOfPages = numberOfPages;
       onLoadMore();
     }
   }
+
+  // void _handleScrollListner() {
+  //   final double max = scrollController.position.maxScrollExtent;
+  //   final double viewport = scrollController.position.viewportDimension;
+  //   final double before = scrollController.position.extentBefore;
+  //   final int numberOfPages = max ~/ viewport;
+  //   final int currentPage = before ~/ viewport;
+  //   if (currentPage / numberOfPages >= percent &&
+  //       !scrollController.position.outOfRange &&
+  //       lastNumberOfPages != numberOfPages) {
+  //     lastNumberOfPages = numberOfPages;
+  //     onLoadMore();
+  //   }
+  // }
 
   void onLoadMore();
 }
